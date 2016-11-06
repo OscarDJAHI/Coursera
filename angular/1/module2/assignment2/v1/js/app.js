1cd5f0c9f5ede991f5ecbbb5ab1bb3d71bc2e4a4
Oscard <djahiherve@yahoo.fr>
2016-11-06 19:30:09 +0100

    code updated

--- angular/1/module2/assignment2/v1/js/app.js
+++ angular/1/module2/assignment2/v1/js/app.js
@@ -1,59 +1,74 @@
 (function(){
 
-	"use strict";
+  "use strict";
 
-  var app = angular.module('shopApp',[]);
-  app.controller("ShopCtrl",ShopCtrl);
-  
-  ShopCtrl.$inject = ["$scope"];
+  var app = angular.module('ShoppingListCheckOff',[]);
 
-  function ShopCtrl($scope){
+  app.controller('ToBuyController', ToBuyController)
+  app.controller('AlreadyBoughtController', AlreadyBoughtController)
+  app.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-    var qty = 20;
 
-    $scope.store = {
-        toBuy :[{ name: "Cookies", quantity: qty, "boughtBtn":false},
-                 { name: "Cakes", quantity: qty, "boughtBtn":false},
-                 { name: "Pinutts", quantity: qty, "boughtBtn":false},
-                 { name: "Pizzas", quantity: qty, "boughtBtn":false},
-                 { name: "Tomatoes", quantity: qty, "boughtBtn":false}
-                ],
-        bought :[]
-    };
-    
-    $scope.toBuy = function(item,qty){
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var that = this;
 
-      // show bought btn before changing of item value
-      showBoughtButton(item);
+    that.items = ShoppingListCheckOffService.getItems();
 
-      item = $scope.store.toBuy[item].quantity;
-      updateQty(item,qty);
+    var toBuy_arr = that.items.toBuy;
+    var bought_arr = that.items.bought;
+
+
+    that.moveInboughtList = function(itemIndex){
+      addBoughtProduct(itemIndex);
+      removeBoughtProduct(itemIndex);    
     };
 
-    $scope.moveInboughtList = function(item){
-      var obj = {name :$scope.store.toBuy[item].name, quantity:$scope.store.toBuy[item].quantity};
-      var arr = $scope.store.bought;
-      addItem(obj,arr);
-
-      arr = $scope.store.toBuy;
-      removeItem(item, arr);
-    };
-    
-    var showBoughtButton = function(item){
-        $scope.store.toBuy[item].boughtBtn = true;
-    };
-    
-    var updateQty = function(item, qty){
-        item = qty;
+    var addBoughtProduct = function(itemIndex){
+      var obj = {name : toBuy_arr[itemIndex].name, quantity:toBuy_arr[itemIndex].quantity};
+      var arr = bought_arr;
+      ShoppingListCheckOffService.addItem(obj,arr);
     };
 
-    var addItem = function(obj,arr){
-        arr.push(obj);
+    var removeBoughtProduct = function(itemIndex){
+      ShoppingListCheckOffService.removeItem(itemIndex, toBuy_arr);
     };
+  }
 
-    var removeItem = function(item,arr){
-      arr.splice(item,1);
-    };
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var that = this;
+
+    that.items = ShoppingListCheckOffService.getItems();
+
+  }
+
+  function ShoppingListCheckOffService() {
+      var service = this;
+
+      var qty = 10;
+
+      var store = {
+          toBuy :[{ name: "Cookies", quantity: qty},
+                   { name: "Cakes", quantity: qty},
+                   { name: "Pinutts", quantity: qty},
+                   { name: "Pizzas", quantity: qty},
+                   { name: "Tomatoes", quantity: qty}
+                  ],
+          bought :[]
+      };
+
+      service.getItems = function () {
+        return store;
+      };
+
+      service.addItem = function(obj,arr){
+          arr.push(obj);
+      };
+
+      service.removeItem = function(itemIdex,arr){
+        arr.splice(itemIdex,1);
+      };
   }
 
 })();
